efd4a663450210189e1242d9f2097cb0418db01f
Maciej Warszawski <maciej@warszawski.pro>
2015-12-08 21:14:26 +0100

    Sample data

---
+++ Database/Database/DataMigrations/03 Application Sample Data/0001 After deployment operational data.sql
@@ -0,0 +1,4 @@
+INSERT INTO [dbo].[Customers] ([Id], [CustomerTypeId], [Name]) VALUES (1, 1, N'John Doe')
+INSERT INTO [dbo].[Customers] ([Id], [CustomerTypeId], [Name]) VALUES (2, 1, N'Andy Smith')
+INSERT INTO [dbo].[Customers] ([Id], [CustomerTypeId], [Name]) VALUES (3, 2, N'Steven Johnson')
+INSERT INTO [dbo].[Customers] ([Id], [CustomerTypeId], [Name]) VALUES (4, 3, N'Amy O''neill')
