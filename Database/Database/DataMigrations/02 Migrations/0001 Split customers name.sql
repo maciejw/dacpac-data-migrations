ad0a05af349c19851a864062c4ca9c94b03ae713
Maciej Warszawski <maciej@warszawski.pro>
2015-12-09 01:10:27 +0100

    Remove obsolete customer name

--- Database/Database/DataMigrations/02 Migrations/0001 Split customers name.sql
+++ Database/Database/DataMigrations/02 Migrations/0001 Split customers name.sql
@@ -1,3 +1,1 @@
-﻿update dbo.Customers set 
-	FirstName = LEFT(Name, CHARINDEX(' ', Name) - 1),
-	LastName = RIGHT(Name, LEN(Name) - CHARINDEX(N' ', Name))
+﻿
--- Database/Database/Schema/dbo.Customers.sql
+++ Database/Database/Schema/dbo.Customers.sql
@@ -1,9 +1,8 @@
 CREATE TABLE [dbo].[Customers] (
     [Id]             INT            NOT NULL,
     [CustomerTypeId] INT            NOT NULL,
-    [Name]           NVARCHAR (255) NULL, 
-    [FirstName] NVARCHAR(255) NULL, 
-    [LastName] NVARCHAR(255) NULL, 
+    [FirstName] NVARCHAR(255) NOT NULL, 
+    [LastName] NVARCHAR(255) NOT NULL, 
     CONSTRAINT [FK_Customers_ToTable] FOREIGN KEY ([CustomerTypeId]) REFERENCES [CustomerTypes]([Id]),
 );
 
